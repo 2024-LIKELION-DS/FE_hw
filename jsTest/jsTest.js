f5cb114faeb60a9e1f18005ae018d029ba45ffb6
mogg22 <ahrthdus1@gmail.com>
2024-05-27 21:09:01 +0900

    [0527] JS 시험 채점 결과

--- jsTest/jsTest.js
+++ jsTest/jsTest.js
@@ -1,32 +1,35 @@
-// #1
+//채점 결과 1점/8점
+
+// #1 (O)
 lion=() => "Hello LikeLion12";
 
-// #2
+// #2 (x)
 let input = prompt("숫자를 입력하세요!!")
 let number = parseInt(input, 10);
 let output = (number%2) = 1 ? "홀수" : "짝수";
 let q2Alert = alert(number + "는" + output + "입니다.");
 
-// #3
+// #3 (X)
 do {
     let q3 = prompt("100을 초과하는 숫자를 입력해주세요.");
 } while (q3 > 100);
 
-// #4
+// #4 (X)
 let str = prompt("문자열을 입력하세요:");
 var result1 = str.split("i");
 console.log(result[1]);
 
-// #5
+// #5 (X)
 const date = new Date('May 27, 2022 0:30');
 let q5Alert = alert(date);
 
-// #6
+// #6 (X)
 function getSecondsToday() {
 
 }
 
-// #7
+// #7 (X)
 let q7result = Math.random()*100;
 console.log("첫번째 랜덤 난수")
 
+// #8 (X)
