c8099c760c680fe538a85b018d184453823315f0
khjhyunji <khjhyunji2003@naver.com>
2024-05-27 21:28:11 +0900

    [0527] JS 시험 채점 결과

--- jsTest/jsTest.js
+++ jsTest/jsTest.js
@@ -1,62 +1,64 @@
-// #1
-// lion = () => {
-//   return "Hello LikeLion12";
-// };
+//총점 7점
 
-// #2
-// let input = prompt("숫자를 입력하세요!!");
+// #1 (정답)
+lion = () => {
+    return "Hello LikeLion12";
+};
 
-// let number = parseInt(input, 10);
+// #2 (정답)
+//let input = prompt("숫자를 입력하세요!!");
 
-// if (number % 2 == 0) {
-//   alert(`${number}는 짝수입니다.`);
-// } else {
-//   alert(`${number}는 홀수입니다.`);
-// }
+//let number = parseInt(input, 10);
 
-// #3
-// do {
-//   var input = prompt("100을 초과하는 숫자를 입력해주세요.");
-//   var number = parseInt(input, 10);
-// } while (number < 100);
+if (number % 2 == 0) {
+    alert(`${number}는 짝수입니다.`);
+} else {
+    alert(`${number}는 홀수입니다.`);
+}
 
-// #4
-// let str = prompt("문자열을 입력하세요: ");
-// console.log(str.slice(2, 5));
+// #3 (정답)
+do {
+    var input = prompt("100을 초과하는 숫자를 입력해주세요.");
+    var number = parseInt(input, 10);
+} while (number < 100);
 
-// #5
-// var date = new Date(2022, 4, 27, 0, 30);
-// alert(date.toLocaleString());
+// #4 (정답)
+let str = prompt("문자열을 입력하세요: ");
+console.log(str.slice(2, 5));
 
-// #6
-// var date = new Date();
-// var now = date.getTime();
-// function getSecondsToday(params) {
-//   alert(now);
-// }
-// getSecondsToday();
+// #5 (정답)
+var date = new Date(2022, 4, 27, 0, 30);
+alert(date.toLocaleString());
 
-// #7
-// function randonNum() {
-//   var number = Math.floor(Math.random() * 100 + 1);
-//   return number;
-// }
+// #6 (오답)
+var date = new Date();
+var now = date.getTime();
+function getSecondsToday(params) {
+    alert(now);
+}
+getSecondsToday();
 
-// document.write(`첫 번째 랜덤 난수: ${randonNum()}`);
-// document.write(`두 번째 랜덤 난수: ${randonNum()}`);
-// document.write(`세 번째 랜덤 난수: ${randonNum()}`);
+// #7 (정답)
+function randonNum() {
+    var number = Math.floor(Math.random() * 100 + 1);
+    return number;
+}
 
-// #8
-// function change() {
-//   var btn = document.getElementById("name");
+document.write(`첫 번째 랜덤 난수: ${randonNum()}`);
+document.write(`두 번째 랜덤 난수: ${randonNum()}`);
+document.write(`세 번째 랜덤 난수: ${randonNum()}`);
 
-//   var num = btn.innerText.slice(9);
-//   var numInt = parseInt(num, 10);
-//   var number = numInt + 1;
+// #8 (정답)
+function change() {
+    var btn = document.getElementById("name");
 
-//   if (number == 4) {
-//     number = 1;
-//   }
+    var num = btn.innerText.slice(9);
+    var numInt = parseInt(num, 10);
+    var number = numInt + 1;
 
-//   btn.textContent = "likelion_" + number;
-// }
+    if (number == 4) {
+        number = 1;
+    }
+
+    btn.textContent = "likelion_" + number;
+}
