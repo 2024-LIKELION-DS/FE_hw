c305c5ee558be700901402937d08af6105a7d195
mogg22 <ahrthdus1@gmail.com>
2024-05-27 22:19:09 +0900

    [0527] JS 시험 채점 결과

--- jsTest/jsTest.js
+++ jsTest/jsTest.js
@@ -1,7 +1,9 @@
-//Q1
+// 채점 결과: 3점/8점
+
+//Q1 (O)
 let lion = () => "Hello LikeLion12";
 
-//Q2
+//Q2 (O)
 let input = prompt("숫자를 입력하세요!!");
 let number = parseInt(input, 10);
 let result =
@@ -9,27 +11,27 @@ let result =
     ? alert(number + "는 짝수입니다.")
     : alert(number + "는 홀수입니다.");
 
-//Q3
+//Q3 (X)
 do {
   let num_input = prompt("100을 초과하는 숫자를 입력해주세요.");
 } while (num_input <= 100);
 
-//Q4
+//Q4 (X)
 let str = prompt("문자열을 입력하세요:");
 console.log(str);
 
-//Q5
+//Q5 (O)
 let date = new Date("May 27, 2022 12:30:00");
 alert(date);
 
-//Q6
+//Q6 (X)
 const now = now.toISOString();
 const today = now.toISOString().split("T")[0];
 let getSecondsToday = () => (seconds = ((now - today) / 1000) % 60);
 
 alert(getSecondsToday);
 
-//Q7
+//Q7 (X)
 
 function randomNum() {
   "첫번째 랜덤 난수 : " + Math.random(0, 100);
@@ -39,5 +41,5 @@ function randomNum() {
 
 document.write(randomNum());
 
-//Q8
+//Q8 (X)
 //죄송합니다... 시간이 부족해 못풀었습니다...
