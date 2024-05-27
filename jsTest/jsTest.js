51c9de90d2babba30c662a5b19dc520d9b8c7580
JungHyunHann <jade20031229@gmail.com>
2024-05-27 20:32:14 +0900

    [0527]JS 시험 답안 제출

---
+++ jsTest/jsTest.js
@@ -0,0 +1,18 @@
+// # 1
+lion = () => "Hello LikeLion12";
+
+// # 2
+function judge() {
+    let input =
+    let number = parseInt(input, 10);
+    let result = 
+}
+
+// # 3
+do {
+    prompt("100을 초과하는 숫자를 입력해주세요.")
+} while (i > 100)
+
+// # 4
+let str = prompt("문자열을 입력하세요");
+console.log(str.split(2,4))
