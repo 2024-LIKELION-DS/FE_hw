2eb563ce14783458521ded3f00df735d75bcba91
dusskdus <skdusb0007@naver.com>
2024-05-27 20:34:28 +0900

    [0527]JS 시험 답안 제출

---
+++ Js test/jsTest.js
@@ -0,0 +1,30 @@
+//Q1
+lion = () => "Hello LikeLion12";
+
+//Q2
+function showAlert() {
+    prompt("숫자를 입력하세요!");
+    let input = ""
+let number = parseInt(input, 10);
+let result =
+}
+//Q3
+function showAlert() {
+    prompt("100을 초과하는 숫자를 입력해주세요.");
+    do {
+        document.write(i);
+        i++;
+    } while (i => 100);}
+
+//Q4
+function inputStr() {
+    prompt("문자열을 입력하세요:");
+    let str = "likelion"
+    console.log(str);}
+
+//Q6
+function getSecondsToday(){
+    const date = getTime(2024,05,27)
+alert(date);
+}
+getSecondsToday();
