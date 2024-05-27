3119db01774af96d14da6da17433fca39810c830
tjsdn04 <sunwoo-0104@naver.con>
2024-05-27 20:32:34 +0900

    [0527] JS 시험 답안 제출

---
+++ jsTest/jsTest.js
@@ -0,0 +1,12 @@
+/*
+#1
+let lion = () => "Hello LikeLion12";
+*/
+let input = prompt();
+let number = parseInt(input, 10);
+
+if (input % 2 == 0) {
+  document.write(input + "는 짝수입니다.");
+} else {
+  document.write(input + "는 홀수입니다.");
+}
