89cd4863f23f70ff7d1dafa86ed523421546aa27
ssorni <ssorni727@gmail.com>
2024-05-27 21:03:01 +0900

    [0527] JS시험 채점 결과

--- jsTest/jsTest.js
+++ jsTest/jsTest.js
@@ -1,18 +1,20 @@
-// # 1
+// 총점 : 1/8점
+
+// # 1 => 1점(단, const나 let 붙여야 올바른 문장이 됩니다! 유의해주세요)
 lion = () => "Hello LikeLion12";
 
-// # 2
+// # 2 => 0점
 function judge() {
     let input =
     let number = parseInt(input, 10);
     let result = 
 }
 
-// # 3
+// # 3 => 0점
 do {
     prompt("100을 초과하는 숫자를 입력해주세요.")
 } while (i > 100)
 
-// # 4
+// # 4 => 0점
 let str = prompt("문자열을 입력하세요");
 console.log(str.split(2,4))
