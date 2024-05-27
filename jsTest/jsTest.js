795d0c8e7b7b98053adcb77dddd9014c10dac843
minji0824 <kmj0824mj@gmail.com>
2024-05-27 20:30:44 +0900

    [0527]시험

---
+++ jsTest/jsTest.js
@@ -0,0 +1,32 @@
+// #1
+lion=() => "Hello LikeLion12";
+
+// #2
+let input = prompt("숫자를 입력하세요!!")
+let number = parseInt(input, 10);
+let output = (number%2) = 1 ? "홀수" : "짝수";
+let q2Alert = alert(number + "는" + output + "입니다.");
+
+// #3
+do {
+    let q3 = prompt("100을 초과하는 숫자를 입력해주세요.");
+} while (q3 > 100);
+
+// #4
+let str = prompt("문자열을 입력하세요:");
+var result1 = str.split("i");
+console.log(result[1]);
+
+// #5
+const date = new Date('May 27, 2022 0:30');
+let q5Alert = alert(date);
+
+// #6
+function getSecondsToday() {
+
+}
+
+// #7
+let q7result = Math.random()*100;
+console.log("첫번째 랜덤 난수")
+
