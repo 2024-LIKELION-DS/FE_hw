bde2efb7b22bb267b0b9d94a65fcc8ea6ff99cf3
ssorni <ssorni727@gmail.com>
2024-05-27 21:36:10 +0900

    [0527] JS시험 채점 결과

--- jsTest/jsTest.js
+++ jsTest/jsTest.js
@@ -1,10 +1,11 @@
-//1
+// 총점 : 1/8점
+//1 => 1점
 lion = function () {
   return "Hello LikeLion12";
 };
 let lion = () => "Hello LikeLion12";
 
-//2
+//2 => 0점
 let input = prompt("숫자를 입력하세요!!!");
 let number = parseInt(input, 10);
 let result = () => {
@@ -15,12 +16,13 @@ let result = () => {
   }
 };
 
-//3
+//3 => 0점
 do {
   let num = prompt("100을 초과하는 숫자를 입력해주세요.");
 } while (num >= 100);
 
-//4
+//4 => 0점 
+// 피드백 : slice를 이용해서 풀어주세요!
 let str = prompt("문자열을 입력하세요 : ");
 let num4 = () => {
   if ((str = "likelion")) {
@@ -28,7 +30,7 @@ let num4 = () => {
   }
 };
 
-//5
+//5 => 0점
 let date = new Date();
 let setFullYear = setFullYear("2022");
 let setMonth = setMonth("5");
@@ -51,20 +53,20 @@ let art = () => {
   );
 };
 
-//6
+//6 => 0점
 function getSecondsToday() {
   let date = new Date();
   let result = getTime(date);
   return alert(result);
 }
 
-//7
+//7 => 0점
 function randomNum() {
   for (let i = 1; i < 11; i++) num = Math.random(0, 101);
   document.write(i + "번째 랜덤 난수 : ", random_num);
 }
 
-//8
+//8 => 0점
 function change() {
   const btn = document.getElementById("name");
   let count = 0;
