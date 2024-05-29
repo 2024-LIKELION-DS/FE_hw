0b2aedaadfe073d7fdd93ac53a33dfe27769e225
yeon-yeon1 <nojk0106@naver.com>
2024-05-29 20:37:28 +0900

    [0529] jsTest 오답 정리

--- jsTest/jsTest.js
+++ jsTest/jsTest.js
@@ -11,35 +11,53 @@ let result =
     ? alert(number + "는 짝수입니다.")
     : alert(number + "는 홀수입니다.");
 
-//Q3 (X)
+//Q3 (X) - 오답 수정 완
+let num_input;
 do {
-  let num_input = prompt("100을 초과하는 숫자를 입력해주세요.");
+  num_input = parseInt(prompt("100을 초과하는 숫자를 입력해 주세요."), 10);
 } while (num_input <= 100);
 
-//Q4 (X)
+//Q4 (X) - 오답 수정 완
 let str = prompt("문자열을 입력하세요:");
-console.log(str);
+console.log("ke1");
 
 //Q5 (O)
 let date = new Date("May 27, 2022 12:30:00");
 alert(date);
 
-//Q6 (X)
-const now = now.toISOString();
-const today = now.toISOString().split("T")[0];
-let getSecondsToday = () => (seconds = ((now - today) / 1000) % 60);
+//Q6 (X) - 오답 수정 완
+function getSecondsToday() {
+  let now = new Date();
+  let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 
-alert(getSecondsToday);
+  let milli_seconds = now - today;
+  return Math.floor(milli_seconds / 1000);
+}
+
+let secondsToday = getSecondsToday();
+alert(secondsToday);
 
 //Q7 (X)
 
 function randomNum() {
-  "첫번째 랜덤 난수 : " + Math.random(0, 100);
-  "두번째 랜덤 난수 : " + Math.random(0, 100);
-  "세번째 랜덤 난수 : " + Math.random(0, 100);
+  return Math.floor(Math.random() * 101);
 }
 
-document.write(randomNum());
+let firstRandomNum = randomNum();
+let secondRandomNum = randomNum();
+let thirdRandomNum = randomNum();
+document.write("첫번째 랜덤 난수 : " + firstRandomNum);
+document.write("두번째 랜덤 난수 : " + secondRandomNum);
+document.write("세번째 랜덤 난수 : " + thirdRandomNum);
 
 //Q8 (X)
-//죄송합니다... 시간이 부족해 못풀었습니다...
+function change() {
+  var elem = document.getElementById("name");
+  if (elem.innerHTML === "likelion_1") {
+    elem.innerHTML = "likelion_2";
+  } else if (elem.innerHTML === "likelion_2") {
+    elem.innerHTML = "likelion_3";
+  } else {
+    elem.innerHTML = "likelion_1";
+  }
+}
