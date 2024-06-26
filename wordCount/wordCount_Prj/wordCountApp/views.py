81d82f82f84d40e8579cdf4010fee09fb28397c2
jinhyo0 <kjhyo0220@gmail.com>
2024-06-26 14:54:33 +0900

    [0626]wordCount 실습

---
+++ wordCount/wordCount_Prj/wordCountApp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+  return render(request, "index.html")
+
+def wordCount(request):
+  return render(request,"wordCount.html")
+
+def result(request):
+  entered_text = request.GET['fulltext']
+  word_list = entered_text.split()
+
+  sum = len(word_list)
+
+  word_dictionary = {}
+
+  for word in word_list:
+    if word in word_dictionary:
+      word_dictionary[word] += 1
+    else:
+      word_dictionary[word] = 1
+
+  return render(request, "result.html", {'alltext': entered_text, 'dictionary': word_dictionary.items(),'sum':sum})
